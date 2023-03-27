81645cba4b5d1594a587cb31fddeb473bfe46cf6
Aman <amank6209@gmail.com>
2023-03-27 14:41:57 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-  return (
-    <div id="main">
-      <div className="date-time"></div>
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.date.toLocaleTimeString()}</h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
